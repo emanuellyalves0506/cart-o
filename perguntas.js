ab2ff478a4e9b886b8150a7413c45f8af20093f9
emanuellyalves0506 <149196266+emanuellyalves0506@users.noreply.github.com>
2024-10-07 11:01:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'cultura',
+    'Quais são os tipos de cultura?',
+    'cultura popular, cultura erudita e cultura de massa',
+)
+
+criaCartao(
+    'cultura',
+    'no que a cultura ajuda?',
+    'Ajuda a reconhecer e a respeitar as diferentes manifestações que moldam a identidade do povo'
+)
+
+criaCartao(
+    'cultura',
+    'Quais grupos a cultura foi influenciada?',
+    'Colonos portugueses, indígenas, negros escravizados e europeus'
+)
+
+criaCartao(
+    'cultura',
+    'Quais os elementos da cultura?',
+    'idioma, religião, música, maneira de se vestir'
+)
+
+criaCartao(
+    'cultura',
+    'como a cultura se manifesta?',
+    'Através de genẽros músicas, estilos de interpretação e práticas de consumo'
+)
+
+criaCartao(
+    'cultura',
+    'como surgiu a cultura?',
+    'surgiu no século XIII e tem origem latina'
+)
+
+criaCartao(
+    'cultura',
+    'o que é cultura culinária?',
+    'é o conjunto de práticas e saberes históricos, culturas, ambientais e territoriais'
+)
+
+criaCartao(
+    'cultura',
+    'Quais as definições de cultura?',
+    'A herança social e total da humanidade'
+)
